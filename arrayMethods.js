471c9e1c6c4785189da4c2f9fd66348f2586da95
Will Spencer <willspencer@me.com>
2022-10-05 17:04:30 -0600

    Both Labs Complete

--- arrayMethods.js
+++ arrayMethods.js
@@ -18,7 +18,11 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 // CODE HERE
-const evenNumbers = mixedNumbers.filter()
+const evenNumbers = mixedNumbers.filter(function(element){
+  return element % 2 === 0;
+})
+console.log(evenNumbers)
+
 
 
 
@@ -39,7 +43,10 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-const postTaxPrices // = prices.map(/* Provide Your Callback Here );
+const postTaxPrices  = prices.map(function(element){
+  return element * 1.07
+})
+//console.log(postTaxPrices)
 
 
 
@@ -57,8 +64,10 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 // CODE HERE
-const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
-
+const totalPopulation  = populations.reduce(function(runningTotal, curElement){
+  return runningTotal + curElement
+})
+//console.log(totalPopulation)
 
 
 ////////// PROBLEM 4 //////////
@@ -82,9 +91,10 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 // CODE HERE
-const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
-
-
+const myStrongest  = monstersInYourPocket.filter(function(element){
+  return element.CP > 200;
+})
+console.log(myStrongest)
 
 ////////// PROBLEM 5 //////////
 
@@ -100,8 +110,10 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 */
 
 // CODE HERE
-
-
+const total =  orders.map(function(element){
+  return element.price + (element.price * element.tax)
+});
+console.log(total)
 
 ////////// PROBLEM 6 //////////
 
@@ -120,4 +132,9 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
 */
 
 // CODE HERE
-
+const bobsTotalPurchases = purchases.filter(function(element){
+  return element.owner === "Bob"
+}).reduce(function(arr, element){
+  return arr + element.price
+},0)
+console.log(bobsTotalPurchases)
